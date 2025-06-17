4c0cd3edd3f1023a0a15e01315120056c8bac6f7
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-17 23:15:14 +0530

    completed

---
+++ src/Assignment1/que16.kt
@@ -0,0 +1,11 @@
+package Assignment1
+
+
+fun main(){
+    var name="Espresso"
+    if(name.length>=5){
+        println("$name has mora than 5 letters" )
+    }else{
+        println("coffee name has les than 5 letters")
+    }
+}
