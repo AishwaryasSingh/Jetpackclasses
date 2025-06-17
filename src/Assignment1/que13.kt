157a57cff403eb976ea664921294e92e832c11fe
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-17 23:11:22 +0530

    completed

---
+++ src/Assignment1/que13.kt
@@ -0,0 +1,9 @@
+package Assignment1
+
+fun main(){
+    println("This is the discount table")
+    for (discount in 10..50 step 10){
+        println(" Get your coffee at $discount% off ")
+
+    }
+}
