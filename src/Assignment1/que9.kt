0519ec53c4b440f89d00e99c4da88c2bfb96ad21
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-17 23:00:59 +0530

    completed

---
+++ src/Assignment1/que9.kt
@@ -0,0 +1,11 @@
+package Assignment1
+
+
+fun main(){
+    val age=50
+    if(age>=60){
+        println("age $age is eligible for senior  discount ")
+    }else{
+        println(" Oops! age $age is  not eligible for senior discount")
+    }
+}
