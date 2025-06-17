1fd56e85570732518a021397e8d601ab22a9a175
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-17 23:19:29 +0530

    completed

---
+++ src/Assignment1/que19.kt
@@ -0,0 +1,14 @@
+package Assignment1
+
+
+fun main() {
+    var orderCount = 1
+    while (true) {
+        println("Processing order #$orderCount ")
+        if (orderCount == 5) {
+            println("Rush hour!")
+            break
+        }
+        orderCount++
+    }
+}
