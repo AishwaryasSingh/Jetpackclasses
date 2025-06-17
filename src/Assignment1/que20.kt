40099f44111a11e8879b760877aa4178a90bb70f
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-17 23:20:32 +0530

    completed

---
+++ src/Assignment1/que20.kt
@@ -0,0 +1,10 @@
+package Assignment1
+
+fun main() {
+    for (orderNumber in 1..10) {
+        if (orderNumber % 2 == 0) {
+            continue
+        }
+        println("Serving order $orderNumber (Regular)")
+    }
+}
