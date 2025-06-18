02119c27768714c0ff2b98e62c4a725f2268e8a4
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-18 14:41:48 +0530

    completed

---
+++ src/Assignment2/q2.kt
@@ -0,0 +1,10 @@
+package Assignment2
+
+
+fun main() {
+    println(totalPrice(5, 5.5))
+    println(totalPrice(10, null))
+}
+fun totalPrice(quantity: Int, pricePerCup: Double?):Double {
+    return quantity * (pricePerCup ?: 0.0)
+}
