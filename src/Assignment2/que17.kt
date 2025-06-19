eb108327a673526b7d19043a129e261989c1362c
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-19 13:28:37 +0530

    completed

---
+++ src/Assignment2/que17.kt
@@ -0,0 +1,11 @@
+package Assignment2
+fun main() {
+    val coffeeBeans = listOf(
+        CoffeeBean("Arabica", "Light"),
+        CoffeeBean("Robusta", "Medium"),
+        CoffeeBean("Liberica", "Dark"),
+        CoffeeBean("Excelsa", "Medium")
+    )
+    println(coffeeBeans.joinToString(separator = "\n"))
+}
+data class CoffeeBean(val name: String, val roastLevel: String)
