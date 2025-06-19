6fcfca7492a547d93b0e8af806e503843a932e33
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-19 13:31:42 +0530

    completed

---
+++ src/Assignment2/que19.kt
@@ -0,0 +1,13 @@
+package Assignment2
+fun main() {
+    val machine = CoffeeMachine("On")
+    machine.status = "On"
+    machine.brewCoffee()
+}
+class CoffeeMachine(var status: String) {
+    fun brewCoffee() {
+        println(if (status == "On") "Brewing coffee" else "Can't brew: machine is $status")
+    }
+
+}
+
