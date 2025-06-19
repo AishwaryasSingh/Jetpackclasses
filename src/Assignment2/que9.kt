5bb5bf8f17663fb0df181a76852a19a2d3be608b
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-19 12:54:10 +0530

    completed

---
+++ src/Assignment2/que9.kt
@@ -0,0 +1,11 @@
+package Assignment2
+
+fun main() {
+    val coffees = listOf("Espresso","Cappuccino","Latte", "Cortado", "Mocha","Cold Brew","Americano")
+    val Coffee = CoffeeStartingWithC(coffees)
+    println("Coffees starting with C are : $Coffee")
+}
+fun CoffeeStartingWithC(coffeeList: List<String>): List<String> {
+    return coffeeList.filter { coffee -> coffee.startsWith("C", ignoreCase = true)
+    }
+}
