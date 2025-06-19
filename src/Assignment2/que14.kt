69391123519bedbdc0eaa41d5425eb99cc3a9b67
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-19 13:25:25 +0530

    completed

---
+++ src/Assignment2/que14.kt
@@ -0,0 +1,14 @@
+package Assignment2
+fun main() {
+    val coffeePrices = listOf(250.0, 300.0, 350.0, 400.0)
+    val discount = 10.0
+
+    val discountedPrices = Prices(coffeePrices, discount)
+
+    println("Original prices: $coffeePrices")
+    println("prices after $discount % discount are: $discountedPrices")
+}
+fun Prices(prices: List<Double>, Percentage: Double): List<Double> {
+    return prices.map { price -> price * (1 -Percentage / 100)
+    }
+}
