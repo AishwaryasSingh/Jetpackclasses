181993e5ba888903dac7bfcf3384cf1ac2a1815a
Aishwarya Singh <aishwaryasingh3666@gmail.com>
2025-06-19 13:01:01 +0530

    completed

---
+++ src/Assignment2/que13.kt
@@ -0,0 +1,9 @@
+package Assignment2
+fun main() {
+    val coffeeOrders = setOf("Latte", "Espresso", "Cappuccino", "Latte", "Mocha","Americano","Mocha","Espresso")
+    Orders(coffeeOrders)
+}
+fun Orders(orders: Set<String>) {
+    val Count = orders.size
+    println("Number of unique coffee orders: $Count")
+}
